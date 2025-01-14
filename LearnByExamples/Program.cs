80bc612381ffdfc18d190b8881c171b4d5b50f87
Vid Mio <vidmio@gmail.com>
2025-01-14 12:39:54 +0100

    Add project files.

---
+++ LearnByExamples/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LearnByExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Miodrag Vidicki!");
+        }
+    }
+}
